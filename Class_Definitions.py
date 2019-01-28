07d28a64530b715f68e11847b2220b4c324941a1
Lena Unger <45045841+LenaUnger@users.noreply.github.com>
2019-01-28 19:53:22 +0100

    Add files via upload

--- Class_Definitions.py
+++ Class_Definitions.py
@@ -1,51 +1,51 @@
 # Import numpy package (numerical Python) for matrix operations
-import numpy
+import numpy as np
 # Import math package for trigonometrical functions
 import math
 # Import scipy.special package for the sigmoid function
 import scipy.special
 
 # Set the way, numpy objects (like matrices, arrays) are displayed to floats with 2 decimals
-numpy.set_printoptions(precision=2, formatter={'float_kind': '{:0.2f}'.format})
+np.set_printoptions(precision=2, formatter={'float_kind': '{:0.2f}'.format})
 
 
 class ForwardKinematics:
     """
     This class contains the calculation of the position of the endeffector with matrix multiplication.
     This class is designed for an articulated manipulator with three revolute joints and four links.
-    The length of link 1 to 3 is set to 1, link 0 has a length of 0. The angles of the revolute
-    joints are variables, defined for each object in the __init__- method. The transformation matrices
+    The length of the links is set for each object in the __init__method. The angles of the revolute
+    joints are variables, being used to calculate the position of the endeffector. The transformation matrices
     follow the Denavit Hartenberg transformation for describing robot kinematics.
     To change the type or orientation of the joints or the link lengths, one needs to alter the
     transformation matrices tn_n-1. To add joints and links, new transformation matrices tn_n-1 need to
     be defined and added to the multiplication of the matrix tn_0.
     """
 
-    def __init__(self, angle1, angle2, angle3):
+    def __init__(self, l0, l1, l2, l3):
         """
-        To initialize an object of this class, the three angles defining the orientation of the
-        revolute joints need to be defined. All three parameters need to be integers in the range
-        [-180, 180].
+        To initialize an object of this class, the four link lengths connecting the joints need to be defined.
         """
         """
-        angle1: angle1 is the angle of the first revolute joint.
-        angle1: angle2 is the angle of the second revolute joint.
-        angle1: angle3 is the angle of the third revolute joint.
+        l0: The length between the basis of the manipulator and the first joint.
+        l1: The length between the first and the second joint of the manipulator.
+        l2: The length between the second and the third joint of the manipulator.
+        l3: The length between the third joint and the endeffector of the manipulator.
         """
-        self.angle_1 = angle1
-        self.angle_2 = angle2
-        self.angle_3 = angle3
+        self.l0 = l0
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
 
         # As the mathematical operations of the math package work with angles of the radiant type,
         # the given degree angles need to be transformed into radiant.
-        self.angle_1 = numpy.deg2rad(self.angle_1)
-        self.angle_2 = numpy.deg2rad(self.angle_2)
-        self.angle_3 = numpy.deg2rad(self.angle_3)
+        #self.angle_1 = numpy.deg2rad(self.angle_1)
+        #self.angle_2 = numpy.deg2rad(self.angle_2)
+        #self.angle_3 = numpy.deg2rad(self.angle_3)
 
 
         pass
 
-    def __calculate__(self):
+    def calculate(self, angle_1, angle_2, angle_3):
         """
         This method contains the calculation of the transformation matrices and the
         position of the endeffector. At first, the transformation matrices from one joint to
@@ -55,34 +55,43 @@ class ForwardKinematics:
         tool center point), the position of the endeffector is described by coordinates of
         the base coordinate system.
         """
+        """
+        angle_1: The angle of the first joint.
+        angle_2: The angle of the second joint.
+        angle_3: The angle of the third joint.
+        """
+
+        angle1 = np.deg2rad(angle_1)
+        angle2 = np.deg2rad(angle_2)
+        angle3 = np.deg2rad(angle_3)
 
         # transformation matrices from link n to link n-1
-        t1_0 = numpy.array([[1, 0, 0, 0],
-                            [0, 1, 0, 0],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]], dtype=float)
-        t2_1 = numpy.array([[math.cos(self.angle_1), 0, math.sin(self.angle_1), 0],
-                            [math.sin(self.angle_1), 0, -(math.cos(self.angle_1)), 0],
-                            [0, 1, 0, 1],
-                            [0, 0, 0, 1]], dtype=float)
-        t3_2 = numpy.array([[-(math.sin(self.angle_2)), -(math.cos(self.angle_2)), 0, -(math.sin(self.angle_2))],
-                            [math.cos(self.angle_2), -(math.sin(self.angle_2)), 0, math.cos(self.angle_2)],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]], dtype=float)
-        t4_3 = numpy.array([[math.cos(self.angle_3), -(math.sin(self.angle_3)), 0, math.cos(self.angle_3)],
-                            [math.sin(self.angle_3), math.cos(self.angle_3), 0, math.sin(self.angle_3)],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]], dtype=float)
+        t1_0 = np.array([[1, 0, 0, self.l0],
+                         [0, 1, 0, 0],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]], dtype=float)
+        t2_1 = np.array([[math.cos(angle1), 0, math.sin(angle1), 0],
+                         [math.sin(angle1), 0, -(math.cos(angle1)), 0],
+                         [0, 1, 0, self.l1],
+                         [0, 0, 0, 1]], dtype=float)
+        t3_2 = np.array([[-(math.sin(angle2)), -(math.cos(angle2)), 0, -self.l2*(math.sin(angle2))],
+                         [math.cos(angle2), -(math.sin(angle2)), 0, self.l2*math.cos(angle2)],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]], dtype=float)
+        t4_3 = np.array([[math.cos(angle3), -(math.sin(angle3)), 0, self.l3*math.cos(angle3)],
+                         [math.sin(angle3), math.cos(angle3), 0, self.l3*math.sin(angle3)],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]], dtype=float)
 
         # Calculation of the transformation matrix from link 4 to 0
         t4_0 = t1_0.dot(t2_1).dot(t3_2).dot(t4_3)
 
         # endpoint is the origin of the coordinate system of the endeffector which has to be
         # described by coordinates of the base coordinate system.
-        endpoint = numpy.array([0, 0, 0, 1])
+        endpoint = np.array([0, 0, 0, 1])
 
         # Calculation of the position of the endeffector in coordinates of the base system.
-        endposition = numpy.dot(t4_0, endpoint)
+        endposition = np.dot(t4_0, endpoint)
 
         # Returning the 4-dimensional vector
         return endposition
@@ -116,15 +125,16 @@ class NeuralNetwork:
         self.onodes = outputnodes
         self.lr = learningrate
 
+
         # The initialized weights are drawn from a normal distribution with the standard deviation being
         # the square root of the number of neurons from the hidden layer (self.wih) or the output layer
         # (self.who)[Rashid, p. 92]. Regarding the size of the matrix: There are as many rows as there
         # are neurons in the layer to which the weights are connected ans as many columns as there are
         # neurons in the layer from where the weights come.
         # self.wih is the weight matrix between the input and the hidden layer.
-        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.wih = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
         # self.who is the weight matrix between the hidden and the output layer.
-        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        self.who = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
 
         # Activation function describes the activation of the neurons. Scipy.special.expit(x) is a sigmoid
         # function, also known as the logistic function, defined as expit(x) = 1/(1+exp(-x)).
@@ -132,7 +142,7 @@ class NeuralNetwork:
 
         pass
 
-    def __train__(self, inputs_list, targets_list):
+    def train(self, inputs_list, targets_list):
         """
         This method contains the training of the neural network. It includes 2 parts:
         First, the inputs are passed through the neural network, being multiplied with the weights
@@ -148,18 +158,18 @@ class NeuralNetwork:
 
         # Converting the input and target lists into arrays with one column and as many
         # rows as there are values, ensuring the functionality of the matrix multiplication.
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        targets = numpy.array(targets_list, ndmin=2).T
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
 
         # The inputs of the hidden layer are the combined and moderated signals, so a matrix
         # multiplication of the weight matrix with the inputs.
-        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_inputs = np.dot(self.wih, inputs)
         # The hidden outputs are the sigmoid function of the hidden inputs.
         hidden_outputs = self.activation_function(hidden_inputs)
 
         # The inputs of the output layer are the combined and moderated signals, so a matrix
         # multiplication of the weight matrix with the hidden outputs.
-        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_inputs = np.dot(self.who, hidden_outputs)
         # The final outputs are the sigmoid function of the final inputs.
         final_outputs = self.activation_function(final_inputs)
 
@@ -167,36 +177,36 @@ class NeuralNetwork:
         output_errors = targets - final_outputs
         # Calculation of the hidden errors splitting up the output errors according to the
         # weights from the hidden to the output layer.
-        hidden_errors = numpy.dot(self.who.T, output_errors)
+        hidden_errors = np.dot(self.who.T, output_errors)
 
         # Updating the weight matrices with the formula coming from the gradient descend method.
         # This formula is derived in the chapter 'the backpropagation algorithm' of the thesis.
-        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
-                                        numpy.transpose(inputs))
-        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
-                                        numpy.transpose(hidden_outputs))
+        self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                     np.transpose(inputs))
+        self.who += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                     np.transpose(hidden_outputs))
 
         pass
 
-    def save(self):
+    def save(self, name):
         '''
         This method stores the trained weight matrices in the file 'saved_wih.npy' (self.wih)
         or 'saved_who.npy' (self.who).
         '''
-        numpy.save('saved_wih.npy', self.wih)
-        numpy.save('saved_who.npy', self.who)
+        np.save(name + '_wih.npy', self.wih)
+        np.save(name + '_who.npy', self.who)
         pass
 
-    def load(self):
+    def load(self, name):
         '''
         This method loads the previously saved weight matrices to either train them further
         or let the trained neural network solve a problem.
         '''
-        self.wih = numpy.load('saved_wih.npy')
-        self.who = numpy.load('saved_who.npy')
+        self.wih = np.load(name + '_wih.npy')
+        self.who = np.load(name + '_who.npy')
         pass
 
-    def __query__(self, inputs_list):
+    def query(self, inputs_list):
         '''
         This method queries the answer of the neural network for a certain input.
         '''
@@ -206,19 +216,20 @@ class NeuralNetwork:
 
         # Converting the input lists into an array with one column and as many rows as
         # there are values, ensuring the functionality of the matrix multiplication.
-        inputs = numpy.array(inputs_list, ndmin=2).T
+        inputs = np.array(inputs_list, ndmin=2).T
 
         # The inputs of the hidden layer are the combined and moderated signals, so a matrix
         # multiplication of the weight matrix with the inputs.
-        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_inputs = np.dot(self.wih, inputs)
         # The hidden outputs are the sigmoid function of the hidden inputs.
         hidden_outputs = self.activation_function(hidden_inputs)
 
         # The inputs of the output layer are the combined and moderated signals, so a matrix
         # multiplication of the weight matrix with the hidden outputs.
-        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_inputs = np.dot(self.who, hidden_outputs)
         # The final outputs are the sigmoid function of the final inputs.
         final_outputs = self.activation_function(final_inputs)
 
         # Returning the final outputs of the neural network.
         return final_outputs
+
